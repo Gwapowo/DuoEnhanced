da88d3790d0b72914a09c39a15a0b6c970510d35
Gwapo <67774050+SupremeTurtle@users.noreply.github.com>
2020-09-10 22:23:50 +0800

    Bap.

---
+++ scripts/boom-duo.js
@@ -0,0 +1,15 @@
+
+const boomRed = newEffect(20, e => {
+	 Draw.color(Color.red, Color.lightGray, e.fin()); 
+	 Lines.stroke(e.fout() * 4); 
+	  Lines.circle(e.x, e.y, e.fin() * 50);
+	  
+	  const boomOrange = newEffect(20, e => {
+	 Draw.color(Color.orange, Color.lightGray, e.fin()); 
+	 Lines.stroke(e.fout() * 4); 
+	  Lines.circle(e.x, e.y, e.fin() * 50);
+	  
+	  const boomGreen = newEffect(20, e => {
+	 Draw.color(Color.green, Color.lightGray, e.fin()); 
+	 Lines.stroke(e.fout() * 4); 
+	  Lines.circle(e.x, e.y, e.fin() * 50);
